d59af86c6dc4922ac12dfb83a8e0f4316b3bb720
NIRAJ JHA <nirajjha655@gmail.com>
2026-07-12 19:45:03 +0545

    Implement emotion detection in emotion_detection.py
    Added emotion detection functionality using Watson API.

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,1 +1,43 @@
 
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"Headers": "string", "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    
+    response = requests.post(url, json = myobj, headers = headers)
+    
+    # Task 7: Handle blank space or network 400 errors
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+        
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+    
+    # Dictionary collection to find the highest-scoring key
+    emotion_dict = {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness}
+    dominant_emotion = max(emotion_dict, key=emotion_dict.get)
+    
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
